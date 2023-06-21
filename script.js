4aff2c16bb837c6853bc05b0d0a57757993b9c6f
bagus320 <bagustbr@gmail.com>
2023-06-21 11:03:29 +0700

    javascript changes

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ function hitung() {
 
   if (unitCount == "under8") {
     result = (factor * dm) / (parseInt(factor) + 12);
+    result = result.toFixed(2) + " Mg";
   } else if (unitCount == "above8") {
     result = (factor * dm) / 20;
+    result = result.toFixed(2) + " Mg";
   } else if (unitCount == "weight") {
     result = (factor * dm) / 70;
+    result = result.toFixed(2) + " Mg";
   } else if (unitCount == "percent") {
     result = (dl * 100) / dm;
+    result = result.toFixed(2) + " %";
   } else {
     result = "000";
   }
   const resultElement = document.getElementById("result");
-  resultElement.innerHTML = result.toFixed(2);
+  resultElement.innerHTML = result;
 }
