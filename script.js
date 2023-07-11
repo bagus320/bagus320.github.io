740669f72b7526d01f3b18a5be5bb3d7938e6e33
bagus320 <bagustbr@gmail.com>
2023-07-11 09:06:42 +0700

    add an animated favicon

--- script.js
+++ script.js
@@ -67,3 +67,79 @@ if (
 ) {
   toggleDarkTheme();
 }
+var favicon_images = [
+  "anim/frame (1).gif",
+  "anim/frame (2).gif",
+  "anim/frame (3).gif",
+  "anim/frame (4).gif",
+  "anim/frame (5).gif",
+  "anim/frame (6).gif",
+  "anim/frame (7).gif",
+  "anim/frame (8).gif",
+  "anim/frame (9).gif",
+  "anim/frame (10).gif",
+  "anim/frame (11).gif",
+  "anim/frame (12).gif",
+  "anim/frame (13).gif",
+
+  "anim/frame (14).gif",
+  "anim/frame (15).gif",
+  "anim/frame (16).gif",
+  "anim/frame (17).gif",
+  "anim/frame (18).gif",
+  "anim/frame (19).gif",
+  "anim/frame (20).gif",
+  "anim/frame (21).gif",
+  "anim/frame (22).gif",
+  "anim/frame (23).gif",
+  "anim/frame (24).gif",
+  "anim/frame (26).gif",
+  "anim/frame (27).gif",
+  "anim/frame (28).gif",
+  "anim/frame (29).gif",
+  "anim/frame (30).gif",
+  "anim/frame (31).gif",
+  "anim/frame (32).gif",
+  "anim/frame (33).gif",
+  "anim/frame (34).gif",
+  "anim/frame (35).gif",
+  "anim/frame (36).gif",
+  "anim/frame (37).gif",
+  "anim/frame (38).gif",
+  "anim/frame (39).gif",
+  "anim/frame (40).gif",
+  "anim/frame (41).gif",
+  "anim/frame (42).gif",
+  "anim/frame (43).gif",
+  "anim/frame (44).gif",
+  "anim/frame (45).gif",
+  "anim/frame (46).gif",
+  "anim/frame (47).gif",
+  "anim/frame (48).gif",
+  "anim/frame (49).gif",
+];
+
+image_counter = 0;
+
+setInterval(function () {
+  // remove current favicon
+  if (document.querySelector("link[rel='icon']") !== null)
+    document.querySelector("link[rel='icon']").remove();
+  if (document.querySelector("link[rel='shortcut icon']") !== null)
+    document.querySelector("link[rel='shortcut icon']").remove();
+
+  // add new favicon image
+  document
+    .querySelector("head")
+    .insertAdjacentHTML(
+      "beforeend",
+      '<link rel="icon" href="' +
+        favicon_images[image_counter] +
+        '" type="image/gif">'
+    );
+
+  // If last image then goto first image
+  // Else go to next image
+  if (image_counter == favicon_images.length - 1) image_counter = 0;
+  else image_counter++;
+}, 55);
