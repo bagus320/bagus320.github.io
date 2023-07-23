3898dd4637643863809f3064122caea243cf48c5
bagus320 <bagustbr@gmail.com>
2023-07-23 12:30:04 +0700

    fixed some bugs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// menyimpan/caching element untuk function autoHide
 const dlLabel = document.querySelector("#dosisLazim");
 const dlInput = document.querySelector("#dl");
 const dmInput = document.querySelector("#dm");
@@ -5,61 +6,112 @@ const dmLabel = document.querySelector("#dmLabel");
 const selectorLabel = document.querySelector("#option-label");
 const unitLabel = document.querySelector("#unitLabel");
 const unitInput = document.querySelector("#factor");
+const selector = document.querySelector("#unitCount");
 const button = document.querySelector("#operation");
 
+//deklarasi object untuk label yang akan ditampilkan
+const labelMap = {
+  under8: {
+    dmLabel: "Dosis Maksimum Dewasa (Farmakope)",
+    unitLabel: "Umur Anak (Dibawah 8 Tahun)",
+  },
+  above8: {
+    dmLabel: "Dosis Maksimum Dewasa (Farmakope)",
+    unitLabel: "Umur Anak (Diatas 8 Tahun)",
+  },
+  weight: {
+    dmLabel: "Dosis Maksimum Dewasa (Farmakope)",
+    unitLabel: "Berat Badan Anak",
+  },
+  month: {
+    dmLabel: "Dosis Maksimum Dewasa (Farmakope)",
+    unitLabel: "Umur Anak (Dalam Bulan)",
+  },
+  percent: {
+    dmLabel: "Dosis Maksimum",
+    unitLabel: "", // Empty string for "percent" case
+  },
+};
+
+//add event listener to the selector element
+// selector.addEventListener("touchstart", autoHide);
+
+// function autoHide yang dipanggil dengan event handler onclick pada element selector
 function autoHide() {
+  // mengambil value selector
   const selector = document.querySelector("#unitCount").value;
-  let show = `display: inline-block;
-  text-align: center;`;
-  let hide = `display: none;`;
 
   switch (selector) {
     case "under8":
-      unitLabel.style = show;
-      unitInput.style = show;
-      unitLabel.innerHTML = "Umur Anak(Dibawah 8 Tahun)";
-      dmLabel.innerHTML = "Dosis Maksimum Dewasa (Farmakope)";
-      break;
     case "above8":
-      unitLabel.style = show;
-      unitInput.style = show;
-      unitLabel.innerHTML = "Umur Anak(Diatas 8 Tahun)";
-      dmLabel.innerHTML = "Dosis Maksimum Dewasa (Farmakope)";
-      break;
     case "weight":
-      unitLabel.style = show;
-      unitInput.style = show;
-      unitLabel.innerHTML = "Berat Badan Anak";
-      dmLabel.innerHTML = "Dosis Maksimum Dewasa (Farmakope)";
-      break;
     case "month":
-      unitLabel.style = show;
-      unitInput.style = show;
-      unitLabel.innerHTML = "Umur Anak(Dalam Bulan)";
-      dmLabel.innerHTML = "Dosis Maksimum Dewasa (Farmakope)";
+      //menambahkan dan menghapus element menggunakan css class
+      unitLabel.classList.remove("hide");
+      unitInput.classList.remove("hide");
+      dlLabel.classList.add("hide");
+      dlInput.classList.add("hide");
+      dmLabel.classList.remove("hide");
+      dmInput.classList.remove("hide");
+      button.classList.remove("hide");
+
+      //mengubah isi text pada label menggunakan object yang dideklarasikan diatas
+      unitLabel.innerHTML = labelMap[selector].unitLabel;
+      dmLabel.innerHTML = labelMap[selector].dmLabel;
       break;
 
     case "percent":
-      unitLabel.style = hide;
-      unitInput.style = hide;
-      dmLabel.innerHTML = "Dosis Maksimum";
+      //menambahkan dan menghapus element menggunakan css class
+      unitInput.classList.add("hide");
+      dlLabel.classList.remove("hide");
+      dlInput.classList.remove("hide");
+      dmLabel.classList.remove("hide");
+      dmInput.classList.remove("hide");
+      button.classList.remove("hide");
+
+      //mengubah isi text pada label menggunakan object yang dideklarasikan diatas
+      unitLabel.innerHTML = labelMap[selector].unitLabel;
+      dmLabel.innerHTML = labelMap[selector].dmLabel;
       break;
 
     default:
-      break;
+      //menyembunyikan semua element input dan label
+      unitLabel.classList.add("hide");
+      unitInput.classList.add("hide");
+      dlLabel.classList.add("hide");
+      dlInput.classList.add("hide");
+      dmLabel.classList.add("hide");
+      dmInput.classList.add("hide");
+      button.classList.add("hide");
   }
 }
-
+// inisialisasi variable yang akan digunakan dibawah
 let result;
 let resultStatement;
 let fixedResult;
+
+//mengambil element div dari html
+const resExp = document.querySelector(".res-exp");
+
+//object yang akan digunakan untuk menambah text pada element resExp
+const labelBuilder = {
+  under8: (factor) => `Dm Anak Dengan Umur ${factor} Tahun Adalah: `,
+  above8: (factor) => `Dm Anak Dengan Umur ${factor} Tahun Adalah: `,
+  weight: (factor) => `Dm Anak Dengan Berat ${factor} Kg Adalah: `,
+  month: (factor) => `Dm Anak Dengan Umur ${factor} Bulan Adalah: `,
+  percent: () => "Persentase dosis adalah: ",
+};
+
+//fungsi utama yang akan menghitung hasil dari input,fungsi dipanggil oleh tombol dengan id operation menggunakan onclick event handler
 function hitung() {
+  //mengambil input dari semua input box
   const dl = parseFloat(document.querySelector("#dl").value);
   const dm = parseFloat(document.querySelector("#dm").value);
   const unitCount = document.querySelector("#unitCount").value;
   const factor = parseFloat(document.querySelector("#factor").value);
   const resultElement = document.querySelector("#result");
   switch (unitCount) {
+    //melakukan kalkulasi dengan case yang diambil dari value pada setiap input
     case "under8":
       result = (factor * dm) / (parseInt(factor) + 12);
       break;
@@ -74,6 +126,8 @@ function hitung() {
 
     case "percent":
       result = (dl * 100) / dm;
+
+      //menambah string pada variable resultStatement berdasarkan kondisional hasil dari kalkulasi diatas
       if (result > 100) {
         resultStatement = "Overdosis Cokk!!";
       } else if (result >= 80 && result <= 100) {
@@ -93,60 +147,58 @@ function hitung() {
       result = "0";
   }
 
-  switch (unitCount) {
-    case "under8":
-    case "above8":
-    case "weight":
-    case "month":
-      result = result.toFixed(2) + " Mg";
-      break;
-    default:
-      result = result.toFixed(2) + " %";
+  //arrow function yang memanggil expLabelMaker object builder untuk menggabungkan string
+  const expLabelMaker = (unitCount, factor) => labelBuilder[unitCount](factor);
+  const expResult = expLabelMaker(unitCount, factor);
+
+  //menambahkan string dari resExp ke element expResult
+  resExp.innerHTML = expResult;
+
+  //if else conditional untuk menambahkan unit satuan pada element result
+  if (unitCount != "percent") {
+    result = result.toFixed(2) + " Mg";
+  } else {
+    result = result.toFixed(2) + " %";
   }
 
+  //menghilangkan string dan mengubah variable result menjadi integer
   fixedResult = parseFloat(result.replace("%", ""));
   if (fixedResult > 0) {
     resultElement.innerText = result;
   } else {
+    //Memasukan String pada element resultElement
     resultElement.innerText = "Diisi yang benerr!!";
   }
+
+  //mengambil element result-statement dari index html
   const resultStatementOutput = document.querySelector("#result-statement");
+  // Mengeluarkan string dari variable resultStatement dan memasukannya kedalam element html resultStatementOutput
   resultStatementOutput.innerText = resultStatement;
   if (unitCount != "percent") {
+    //menyembunyikan element resultStatementOutput
     resultStatementOutput.style.display = "none";
   } else {
     resultStatementOutput.style.display = "block";
   }
 }
 
-let icon = document.getElementById("sun-icon");
+//caching element untuk function toggleDarkTheme
+let icon = document.querySelector("#sun-icon");
 const body = document.body.classList;
-const imageCache = {};
 
 function toggleDarkTheme() {
+  //menambahkan class "dark-theme" pada element body
   body.toggle("dark-theme");
 
+  //akan merubah clickable icon sesuai theme yang dipilih dan menyimpan icon dalam cache
   if (document.body.classList.contains("dark-theme")) {
-    if (imageCache["moon"]) {
-      icon.src = imageCache["moon"];
-    } else {
-      const moonImage = new Image();
-      moonImage.src = "assets/moon.png";
-      imageCache["moon"] = moonImage.src;
-      icon.src = moonImage.src;
-    }
+    icon.src = "assets/moon.png";
   } else {
-    if (imageCache["sun"]) {
-      icon.src = imageCache["sun"];
-    } else {
-      const sunImage = new Image();
-      sunImage.src = "assets/sun_drawing.png";
-      imageCache["sun"] = sunImage.src;
-      icon.src = sunImage.src;
-    }
+    icon.src = "assets/sun_drawing.png";
   }
 }
 
+//memanggil fungsi toggleDarkTheme jika user menggunakan settingan default darkmode
 if (
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme:dark)").matches
